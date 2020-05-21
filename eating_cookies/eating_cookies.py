855775b50d9d136819baa016952b646c6987e5e2
agohorel <3317203+agohorel@users.noreply.github.com>
2020-05-21 15:35:18 -0400

    optimize eating_cookies w/ cache

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,17 +2,31 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
+
+
+def eating_cookies(n, cache=None):
+    # setup
+    if cache == None:
+        cache = [0 for x in range(n+1)]
+    # base cases
     if n == 0:
         return 1
     elif n <= 0:
         return 0
+    # if value is already in cache
+    if cache[n] > 0:
+        return cache[n]
+    # if we still need to compute value
+    elif cache[n] == 0:
+        cache[n] = eating_cookies(
+            n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
 
-    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+    return cache[n]
 
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 5
+    num_cookies = 50
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(
+        f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
